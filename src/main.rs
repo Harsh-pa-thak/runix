42f3c0ece3011ba6095a01706c551647ccbe917b
Harsh Pathak <harphy45@gmail.com>
2026-04-26 23:17:24 +0530

    cli command setup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use clap::Parser;
+#[derive(Parser)]
+#[command(name = "runix")]
+#[command(about = "Terminal Is Looking Good")]
+struct Cli {
+    word: String,
+
+    #[arg(long, default_value = "false")]
+    art: bool,
+}
+
 fn main() {
-    println!("Hello,rUNIX!");
+    let cli = Cli::parse();
+    if cli.art {
+        println!(" the word is art  {}", cli.word);
+    }
+    else{
+        println!("the word is {}", cli.word);
+    }
 
 }
